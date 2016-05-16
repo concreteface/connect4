cf08aba868e58bf026d7b43de91e81bc98d1452a
concreteface <concreteface@users.noreply.github.com>
2016-05-16 10:12:30 -0400

    changed board to cells

--- testgrid.rb
+++ testgrid.rb
@@ -1,5 +1,6 @@
-require_relative 'cell'
+require_relative 'lib/cell'
 require 'pry'
+require 'pp'
 class Grid
   attr_reader :rows, :columns
   attr_accessor :board
@@ -9,46 +10,56 @@ class Grid
   end
 
   def create_board
-    @board = Array.new(rows){Array.new(columns, true)}
+    @board = Array.new(rows){Array.new(columns){Cell.new}}
   end
 
   def can_play?(slot)
-    @board[0][slot] == true
+    @board[0][slot].empty == true
+    # binding.pry
   end
 
   def move(column)
     if can_play?(column)
       empty = nil
       board.each_with_index do |row, i|
-        if row[column] == false
+        if row[column].empty == false
           empty =  i - 1
           break
         else empty = rows - 1
         end
       end
-      board[empty][column] = false
+      board[empty][column].empty = false
     else puts "Please choose another column"
     end
   end
 
   def display
-    board.each {|x| puts x.join" "}
+    # board.each {|x| puts x.join" "}
+    pp board
   end
 end
 
 grid = Grid.new(6,7)
 grid.create_board
 
-grid.move(0)
-# grid.move(0)
-# grid.move(0)
-
-grid.display
-# # puts grid.board[6][0]
+grid.move(6)
+grid.move(6)
+grid.move(6)
+grid.move(6)
+grid.move(6)
+grid.move(6)
+grid.move(6)
+grid.move(6)
+grid.board.display
+# grid.move(6)
+# grid.move(6)
+# grid.move(1)
+# grid.move(1)
+# grid.board[5][0] = 'xxxxxxxxxxxxxxxxxxxxxxx'
 grid.board.each_with_index do |row, j|
   puts "row: #{j + 1}"
   row.each_with_index do |cell, i|
     # puts cell.empty
-    puts "column #{i + 1} empty: #{cell}"
+    puts "column #{i + 1} empty: #{cell.empty}"
   end
 end
